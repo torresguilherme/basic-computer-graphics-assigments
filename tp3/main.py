520f5ac4778c3a33a6c05beddf3476bcc0636d4a
Guilherme Torres <guilhermetorres97@gmail.com>
2019-06-26 10:54:39 -0300

    models/reading md2 header

--- tp3/main.py
+++ tp3/main.py
@@ -19,7 +19,31 @@ class Animation:
 
 class MD2Object:
     def __init__(self, filename):
-        pass
+        with open(filename, 'rb') as f:
+            ### READING HEADER
+            ident = f.read(4)
+            if ident != 'IDP2'.encode('ascii'):
+                print("Nao é um arquivo MD2, numero mágico é: " + ident.decode())
+                return
+            self.version = int.from_bytes(f.read(4), byteorder='little') # 8
+
+            self.skinwidth = int.from_bytes(f.read(4), byteorder='little') # width of texture
+            self.skinheight = int.from_bytes(f.read(4), byteorder='little') # height of texture
+            self.framesize = int.from_bytes(f.read(4), byteorder='little') # size of one frame in bytes
+
+            self.num_skins = int.from_bytes(f.read(4), byteorder='little') # number of textures
+            self.num_vertices = int.from_bytes(f.read(4), byteorder='little') # number of vertices
+            self.num_tex_coords = int.from_bytes(f.read(4), byteorder='little') # number of texture coordinates
+            self.num_tris = int.from_bytes(f.read(4), byteorder='little') # number of triangles
+            self.num_commands = int.from_bytes(f.read(4), byteorder='little') # number of opengl commands
+            self.num_frames = int.from_bytes(f.read(4), byteorder='little') # total number of frames
+
+            self.ofs_skins = int.from_bytes(f.read(4), byteorder='little') # offset to skin names (64 bytes each)
+            self.ofs_st = int.from_bytes(f.read(4), byteorder='little') # offset to s-t texture coordinates
+            self.ofs_tris = int.from_bytes(f.read(4), byteorder='little') # offset to triangles
+            self.ofs_frames = int.from_bytes(f.read(4), byteorder='little') # offset to frame data
+            self.ofs_glcmds = int.from_bytes(f.read(4), byteorder='little') # offset to opengl commands
+            self.ofs_end = int.from_bytes(f.read(4), byteorder='little') # offset to end of file
 
 ###############################
 ### Renderer
